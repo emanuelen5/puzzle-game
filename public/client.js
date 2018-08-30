3cfa9acbf09f68f4cccb5e8cc32c76b384b234f9
Glitch (sliding-photo-puzzle) <none>
2018-08-30 18:59:08 +0000

    🍢💼 Checkpoint ./public/client.js:252452/487 ./public/style.css:252452/72

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
-var img = new Image('https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_114200.jpg?1535486103628')
-myImage.src
+var img = new Image();
+img.src = 'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_114200.jpg?1535486103628'
 
-console.log(img.naturalWidth)
-console.log(img.naturalHeight)
+var gameAspectRatio = img.naturalWidth) / img.naturalHeight
+game-area.style.setProperty('--game-width', gameAspectRatio)
