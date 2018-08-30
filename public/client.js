77c73f40dc968e123c4c9439aa0c28419b1699dd
Glitch (sliding-photo-puzzle) <none>
2018-08-30 21:07:04 +0000

    🎨👶 Checkpoint ./public/client.js:252452/1703

--- public/client.js
+++ public/client.js
@@ -2,19 +2,21 @@ var gameArea = document.getElementById('game-area');
 var img = new Image();
 img.src = 'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_104348.jpg?1535662149619'
 
-var gameAspectRatio = img.naturalWidth / img.naturalHeight
-gameArea.style.setProperty('--img-url', `url(${img.src}`)
+function gameSetup() {
+  var gameAspectRatio = img.naturalWidth / img.naturalHeight
+  gameArea.style.setProperty('--img-url', `url(${img.src}`)
 
-if (gameAspectRatio > 1) {
-  var maxViewportWidth = 90;
-  gameArea.style.setProperty('--game-width', `${maxViewportWidth}vw`)
-  var height = maxViewportWidth / gameAspectRatio
-  gameArea.style.setProperty('--game-height', `${height}vw`)
-} else {
-  var maxViewportHeight = 80;
-  var width = maxViewportHeight * gameAspectRatio
-  gameArea.style.setProperty('--game-width', `${width}vh`)
-  gameArea.style.setProperty('--game-height', `${maxViewportHeight}vh`)
+  if (gameAspectRatio > 1) {
+    var maxViewportWidth = 90;
+    gameArea.style.setProperty('--game-width', `${maxViewportWidth}vw`)
+    var height = maxViewportWidth / gameAspectRatio
+    gameArea.style.setProperty('--game-height', `${height}vw`)
+  } else {
+    var maxViewportHeight = 80;
+    var width = maxViewportHeight * gameAspectRatio
+    gameArea.style.setProperty('--game-width', `${width}vh`)
+    gameArea.style.setProperty('--game-height', `${maxViewportHeight}vh`)
+  }
 }
 
 function createTiles() {
@@ -26,4 +28,22 @@ function createTiles() {
   gameArea.innerHTML = tiles.join("")
 }
 
+function positionImages() {
+  var tilesArray = document.querySelectorAll('tile-container')
+  tilesArray.forEach(function(tile,index) {
+    background-poit
+  })
+.tile-container:nth-of-type(4n+4) {
+  background-position-x: 100%;
+  grid-column: 4/5;
+}
+
+.tile-container:nth-of-type(n+1):nth-of-type(-n+4) {
+  background-position-y: 0%;
+  grid-row: 1/2;
+}
+}
+
+gameSetup()
 createTiles()
+positionImages()
