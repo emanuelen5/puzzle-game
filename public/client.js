fe43d750bc1984e4a2a0df1ed16e574b8c11fdcb
Glitch (sliding-photo-puzzle) <none>
2018-08-30 19:09:18 +0000

    🌺🛍 Checkpoint ./public/client.js:252452/1030 ./public/style.css:252452/110

--- public/client.js
+++ public/client.js
@@ -1,5 +1,17 @@
+var gameArea = document.getElementById('game-area');
 var img = new Image();
 img.src = 'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_114200.jpg?1535486103628'
 
-var gameAspectRatio = img.naturalWidth) / img.naturalHeight
-game-area.style.setProperty('--game-width', gameAspectRatio)
+var gameAspectRatio = img.naturalWidth / img.naturalHeight
+
+if (gameAspectRatio > 1) {
+  var maxViewportWidth = 90;
+  gameArea.style.setProperty('--game-width', `${maxViewportWidth}vw`)
+  var height = maxViewportWidth / gameAspectRatio
+  gameArea.style.setProperty('--game-height', '${height}vw')
+} else {
+  var maxViewportHeight = 80;
+  var width = maxViewportHeight * gameAspectRatio
+  gameArea.style.setProperty('--game-width', `${width}vw`)
+  gameArea.style.setProperty('--game-height', '${maxViewportHeight}vw')
+}
