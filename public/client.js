453a6201698f74937157d8d12381861e3c6a8414
Glitch (sliding-photo-puzzle) <none>
2018-09-03 21:56:03 +0000

    🐡📈 Checkpoint ./public/client.js:252452/241

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 var gameArea = document.getElementById('game-area');
 var img = new Image();
-img.src = 'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_114200.jpg?1535486103628'
+img.src = 'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FPANO_20151007_150454.jpg?1536011113511'
 var tileState = {
   tileLoc: {},
   nullLoc: ''
@@ -13,11 +13,11 @@ function boardSetup() {
   if (gameAspectRatio > 1) {
     var maxViewportWidth = 90;
     gameArea.style.setProperty('--game-width', `${maxViewportWidth}vw`)
-    var height = maxViewportWidth / gameAspectRatio
+    var height = Math.ceil(maxViewportWidth / gameAspectRatio)
     gameArea.style.setProperty('--game-height', `${height}vw`)
   } else {
     var maxViewportHeight = 80;
-    var width = maxViewportHeight * gameAspectRatio
+    var width = Math.ceil(maxViewportHeight * gameAspectRatio)
     gameArea.style.setProperty('--game-width', `${width}vh`)
     gameArea.style.setProperty('--game-height', `${maxViewportHeight}vh`)
   }
