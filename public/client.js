8871e94d0f30a16322ad4c175945e3cd72765e31
Glitch (sliding-photo-puzzle) <none>
2018-09-03 15:31:55 +0000

    👙🌑 Checkpoint ./public/client.js:252452/63

--- public/client.js
+++ public/client.js
@@ -99,8 +99,8 @@ function checkValidMove(tileLoc, nullLoc) {
 }
 
 function drawGame() {
-  tileState['tileLoc'].forEach(function(tile) {
-    
+  tileState['tileLoc'].map(function(tile) {
+    console.log(tile)
   });
 }
 
