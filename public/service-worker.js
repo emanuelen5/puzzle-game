cde277a237c9de2d6264277d91763d7f769bf434
Glitch (sliding-photo-puzzle-staging) <none>
2018-09-17 01:04:41 +0000

    🌨🍢 Checkpoint ./public/service-worker.js:252452/4

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
 var cacheName = 'slidingPuzzlePWA-v0.0';
 var filesToCache = [
-  '/',
+  /*'/',
   '/public/client.js',
   '/public/style.css',
   '/views/index.html',
-  'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_104348.jpg?1535662149619'
+  'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_104348.jpg?1535662149619'*/
 ];
 
 self.addEventListener('install', function(e) {
