782c12135dc2be5f72e3ed21ab33bff88472b7b7
Glitch (sliding-photo-puzzle-staging) <none>
2018-09-21 21:56:01 +0000

    📙👬 Checkpoint ./server.js:252452/1303 ./test/test.js:252452/482 ./public/service-worker.js:252452/6862

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
