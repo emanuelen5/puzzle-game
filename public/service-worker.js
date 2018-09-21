0aa6d24c4c6df7bee64c0071fa732ddd53953df0
Glitch (sliding-photo-puzzle-staging) <none>
2018-09-21 21:02:16 +0000

    🛤💒 Checkpoint ./public/service-worker.js:252452/2265 ./test/test.js:252452/4

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,40 @@
-var cacheName = 'slidingPuzzlePWA-v0.0';
+var cacheName = "slidingPuzzlePWA-v0.0";
 var filesToCache = [
-  '/',
-  '/client.js',
-  '/style.css',
-  'https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_104348.jpg?1535662149619'
+  "/",
+  "/client.js",
+  "/style.css",
+  "https://cdn.glitch.com/24dc13be-ff08-4007-bf38-7c45e0b5d9e1%2FIMG_20180826_104348.jpg?1535662149619"
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+self.addEventListener("install", function(e) {
+  console.log("[ServiceWorker] Install");
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      console.log("[ServiceWorker] Caching app shell");
       return cache.addAll(filesToCache);
-    }) 
-  );
-});
-
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
     })
   );
-  return self.clients.claim()
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
+self.addEventListener("activate", function(e) {
+  console.log("[ServiceWorker] Activate");
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  console.log("[ServiceWorker] Fetch", e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
