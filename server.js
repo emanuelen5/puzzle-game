03b384045ee6a0398f5bdb3701b57b3ef5bf3bbb
Glitch (sliding-photo-puzzle-staging) <none>
2018-10-15 21:20:51 +0000

    Import of sheridanvk/sliding-photo-puzzle

--- server.js
+++ server.js
@@ -2,21 +2,21 @@
 // where your node app starts
 
 // init project
-var express = require("express");
+var express = require('express');
 var app = express();
-
-// we've started you off with Express,
+const port =  process.env.port || 3000;
+// we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+var listener = app.listen(port, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
