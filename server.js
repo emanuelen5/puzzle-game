0deb37587fbe71e82246692532a7dabd0f5e9a9b
Glitch (sliding-photo-puzzle-staging) <none>
2018-10-20 19:11:43 +0000

    📒💎 Checkpoint ./server.js:252452/813

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const port =  process.env.port || 3000;
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// set up a route to redirect http to https
+app.get('*', function(request, response){
+  response.redirect('https://' + request.headers.host + request.url);
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
